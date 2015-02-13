7e663eef6133d3e2091dca5dbc9e6cb1f86df035
Daniel Kershaw <danjamker@gmail.com>
2015-02-13 10:17:12 +0000

    Modify the miner

--- chan.py
+++ chan.py
@@ -1,12 +1,10 @@
 __author__ = 'danielkershaw'
-import praw
-import Tools
+
 from datetime import datetime
 from ChanDB import ChanDB
 import basc_py4chan
 import urllib, json
-#from celeryTasks import mineChan
-import sys
+from celeryTasks import mineChan
 class main:
 
     def __init__(self):
@@ -25,7 +23,7 @@ class main:
                     for tid in thread_ids:
                         if self.db.insert_to_mq(str(tid)+":"+str(b["board"])) == False:
                             print "Adding!!!!!!!"
-                            self.mineChan(b["board"], tid)
+                            mineChan.delay(b["board"], tid)
 
 
 
@@ -35,22 +33,6 @@ class main:
     def stop(self):
         self.run = False
 
-    def mineChan(self, board, thread):
-        try:
-            db = ChanDB()
 
-            url = "https://a.4cdn.org/"+str(board)+"/thread/"+str(thread)+".json"
-            response = urllib.urlopen(url);
-            data = json.loads(response.read())
-
-            # information from the OP
-            for pp in data["posts"]:
-                pp["lancs_id"] = str(board)+":"+str(thread)+":"+str(pp["no"])
-                db.insert_post(pp)
-            db.remove_from_queue(str(thread)+":"+str(board))
-        except Exception, e:
-            print e
-            print "{0} : Unexpected error celeryTasks.py-mineChan: {1} body: {2}".format(datetime.now().strftime("%c"), sys.exc_info()[0], board+":"+thread)
-            raise
 if __name__ == "__main__":
     main().start()
